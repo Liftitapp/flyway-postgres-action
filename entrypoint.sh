9bc06ec4b2ecb923bd641b7c8e15490654ea15c8
Juan Pablo Arias <jpa3211@gmail.com>
2021-05-11 10:22:51 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,9 @@
 
 echo $INPUT_FLYWAY_CONF_SHA | base64 -d > /flyway/conf/flyway.conf
 
+echo "Workspace:"
 ls $GITHUB_WORKSPACE
+echo "SQL Folder:"
 ls $GITHUB_WORKSPACE/$INPUT_FLYWAY_SQL
 cp -rf $GITHUB_WORKSPACE/$INPUT_FLYWAY_SQL/* /flyway/sql
 
@@ -22,3 +24,5 @@ flyway info
 cd /flyway/
 echo $INPUT_POST_MIGRATION_BASH_SHA | base64 -d > post_migration.sh
 bash post_migration.sh
+
+ls /flyway/sql
